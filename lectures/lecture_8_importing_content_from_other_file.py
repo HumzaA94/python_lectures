74bf6cc05279634b2e0ba6a4eeec41f9c6594e09
Humza Ahmed <humza.ahmed@sportsiq.ai>
2021-12-10 16:43:30 -0500

    adding in lectures 8, 9 and 10

---
+++ lectures/lecture_8_importing_content_from_other_file.py
@@ -0,0 +1,4 @@
+from python_lectures.lectures.lecture_8_introduction_to_python import calculation
+
+if __name__ == '__main__':
+    plus, minus, multiply, division, _ = calculation(30, 40)
---
+++ lectures/lecture_8_introduction_to_python.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+# Global Variables
+bool_variable = True
+int_variable = 1
+string_variable = "word"
+float_variable = 1.0
+
+
+def ask(name, age=""):
+    print(f"Name is {name} and I am {age} years old.")
+
+
+def calculation(a, b):
+    # if a and b are actually ints
+    # clue for checking if number, use 'isinstance' function
+    # https://www.w3schools.com/python/ref_func_isinstance.asp
+
+    plus = a + b
+    minus = a - b
+    multiplication = a * b
+    division = a / b
+    formula_for_living_forever = (plus - minus) / multiplication * division
+
+    return plus, minus, multiplication, division, formula_for_living_forever
+
+
+if __name__ == '__main__':
+    ask("Humza", age=29)
+    plus, _, _, _, formula_for_living_forever = calculation(30, 10)
+    _, minus, _, _, formula_for_living_forever = calculation(40, 2)

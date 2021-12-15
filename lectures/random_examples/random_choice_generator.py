44213cdc4e1483015f0c4daa87f2cca2f6b4aca4
Humza Ahmed <humza.ahmed@sportsiq.ai>
2021-12-14 19:29:07 -0500

    new content, new structure

---
+++ lectures/random_examples/random_choice_generator.py
@@ -0,0 +1,13 @@
+import random
+
+from python_lectures.lectures.secret_information import list_
+
+
+# provide the function a list and return back one choice at random from the list
+def random_output():
+    value = random.choice(list_)
+    return print(value)
+
+
+if __name__ == '__main__':
+    random_output()
---
+++ lectures/secret_information.py
@@ -0,0 +1,6 @@
+list_ = ['Ben',
+         'Daman',
+         'Ghassan',
+         'Hailey',
+         'Park',
+         'Rayan']
